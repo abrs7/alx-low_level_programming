5f46f61de17d5e86eec17034641f3339bb2bf62f
Abraham <abrishsambong@gmail.com>
2022-06-13 13:15:17 -0700

    dome

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,21 @@
 #include "main.h"
+
 /**
- *puts2 - prints even characters of a string
- *
- *@str:input
- *
- *Return:void
- *
+ * puts2 - prints every other character of a string.
+ *@str: the string to use.
  */
+
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
