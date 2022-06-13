0b0fbd7490af5eca09916915079dc9a3cc13108d
Abraham <abrishsambong@gmail.com>
2022-06-13 12:42:02 -0700

    this is me

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,17 @@
  */
 void print_rev(char *s)
 {
-int i;
+	int i = 0;
 
-i = 0;
-while(s[i] != '\0')
-{
-i++;
-}
-for(i = i; i>= 0: i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	};
+	_putchar('\n');
 }
